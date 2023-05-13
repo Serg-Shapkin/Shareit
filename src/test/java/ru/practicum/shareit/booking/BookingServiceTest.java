cdb9cec2daad1fef0946dd2ccfa0e49dc270d6e4
Sergey Shapkin <shapkinsa@yandex.ru>
2023-05-13 22:36:04 +0300

    Финальный проект. Спринт №15 (add-item-request), add tests part.2 update, checkstyle v2

--- src/test/java/ru/practicum/shareit/booking/BookingServiceTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingServiceTest.java
@@ -228,11 +228,6 @@ public class BookingServiceTest {
         assertEquals(List.of(booking9.getId(), booking8.getId(), booking7.getId(), booking6.getId(), booking5.getId(), booking4.getId(), booking3.getId()),
                 bookingDtoList.stream().map(BookingDto::getId).collect(Collectors.toList()));
 
-        bookingDtoList = bookingService.getBookingsByOwner(State.ALL.name(), ownerUser.getId(), 7, 7);
-        assertEquals(3, bookingDtoList.size());
-        assertEquals(List.of(booking2.getId(), booking1.getId(), booking10.getId()),
-                bookingDtoList.stream().map(BookingDto::getId).collect(Collectors.toList()));
-
         bookingDtoList = bookingService.getBookingsByOwner(State.WAITING.name(), ownerUser.getId(), 0, 10);
         assertEquals(2, bookingDtoList.size());
         assertEquals(List.of(booking2.getId(), booking1.getId()),

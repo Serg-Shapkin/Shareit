6b4dd002b3656f5607032a223898beae6b77c646
Sergey Shapkin <shapkinsa@yandex.ru>
2023-05-14 18:05:28 +0300

    Финальный проект. Спринт №15 (add-item-request), add tests part.7

--- src/main/java/ru/practicum/shareit/exception/ErrorResponse.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorResponse.java
@@ -3,6 +3,8 @@ package ru.practicum.shareit.exception;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 public class ErrorResponse {
@@ -12,5 +14,18 @@ public class ErrorResponse {
     public ErrorResponse(String error, String description) {
         this.error = error;
         this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ErrorResponse that = (ErrorResponse) o;
+        return Objects.equals(error, that.error) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(error, description);
     }
 }

34f959bb2e02f4aa3730826cc80dd0b18493e432
Sergey Shapkin <shapkinsa@yandex.ru>
2023-05-27 17:41:39 +0300

    Финальный проект. Спринт №16 v4

--- gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,9 +1,13 @@
 package ru.practicum.shareit.request.dto;
 
 import lombok.*;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.user.User;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 @AllArgsConstructor
@@ -17,4 +21,8 @@ public class ItemRequestDto {
     @NotBlank(message = "Описание запроса вещи отсутствует или передана пустая строка")
     @Size(max = 200)
     private String description;
+
+    private User requestor;
+    private LocalDateTime created;
+    private List<ItemDto> items;
 }
---
+++ gateway/src/main/java/ru/practicum/shareit/user/User.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.user;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+}
